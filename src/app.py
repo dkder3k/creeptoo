29da4410c1c1cdd0d14b6bc11d98f67bb6c510ee
Nikita Kunets <dkder3k@gmail.com>
2019-05-10 19:45:39 +0300

    Add configurations to run app as docker service

--- src/app.py
+++ src/app.py
@@ -1,20 +1,20 @@
 from flask import Flask, request
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route("/caesar", methods=["POST", "GET"])
+@application.route("/caesar", methods=["POST", "GET"])
 def caesar():
-    from src.caesar import make_shift
+    from caesar import make_shift
     if request.method == "POST":
         return make_shift(request.form["text"], int(request.form["key"]), request.form["direction"])
     else:
         return "Hello, Caesar"
 
 
-@app.route("/gronsfeld", methods=["POST", "GET"])
+@application.route("/gronsfeld", methods=["POST", "GET"])
 def gronsfeld():
-    from src.gronsfeld import make_shift
+    from gronsfeld import make_shift
     if request.method == "POST":
         return make_shift(request.form["text"], int(request.form["key"]), request.form["direction"])
     else:
