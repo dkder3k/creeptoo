97825cc983d8b9a5a42a35a63be648b8cc4582e9
Nikita Kunets <nik.kunets@gmail.com>
2020-02-10 21:42:56 +0300

    Small optimization to reduce memory allocations in gronsfeld adn rot functions

--- app-backend/src/ciphers/gronsfeld.go
+++ app-backend/src/ciphers/gronsfeld.go
@@ -4,14 +4,14 @@ import "unicode"
 
 // Gronsfeld - encrypt/decrypt given text with a key
 func Gronsfeld(text, key string, action int) string {
-	var result []rune
+	result := make([]rune, 0, len(text))
 	for i, c := range text {
 		if unicode.IsLetter(c) {
-			shiftedC := rune(int(c) + (int(key[i%len(key)]) - 48) * action)
+			shiftedC := rune(int(c) + (int(key[i%len(key)])-48)*action)
 			if !unicode.IsLetter(shiftedC) ||
-			unicode.IsUpper(c) && unicode.IsLower(shiftedC) ||
-			unicode.IsLower(c) && unicode.IsUpper(shiftedC) {
-				shiftedC = rune(int(shiftedC) + 26 * action * -1)
+				unicode.IsUpper(c) && unicode.IsLower(shiftedC) ||
+				unicode.IsLower(c) && unicode.IsUpper(shiftedC) {
+				shiftedC = rune(int(shiftedC) + 26*action*-1)
 			}
 			result = append(result, shiftedC)
 		} else {
--- app-backend/src/ciphers/rot.go
+++ app-backend/src/ciphers/rot.go
@@ -5,12 +5,12 @@ import "unicode"
 // Rot - encrypt/decrypt given text with a key
 func Rot(text string, key int, action int) string {
 	key %= 26
-	var result []rune
+	result := make([]rune, 0, len(text))
 	for _, c := range text {
 		if unicode.IsLetter(c) {
-			shiftedC := rune(int(c) + key * action)
+			shiftedC := rune(int(c) + key*action)
 			if !unicode.IsLetter(shiftedC) {
-				shiftedC = rune(int(shiftedC) + 26 * action * -1)
+				shiftedC = rune(int(shiftedC) + 26*action*-1)
 			}
 			result = append(result, shiftedC)
 		} else {
