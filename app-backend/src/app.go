3ee584e7b6d48eca3a34e044289932eb9b6111ad
Nikita Kunets <nik.kunets@gmail.com>
2020-05-17 16:30:37 +0300

    Backend app refactoring
    * changed default path from /v1 to /api/v1
    * combine separate handler functions for Gronsfeld and ROT* ciphers into one apiHandler func

--- app-backend/src/app.go
+++ app-backend/src/app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"errors"
 	"log"
 	"net/http"
 	"os"
@@ -24,8 +25,7 @@ func main() {
 	logger.Println("Server starting...")
 
 	router := http.NewServeMux()
-	router.Handle("/v1/rot", rotHandler())
-	router.Handle("/v1/gronsfeld", gronsfeldHandler())
+	router.Handle("/api/v1/", apiHandler())
 
 	server := &http.Server{
 		Addr:         listenAddr,
@@ -36,8 +36,8 @@ func main() {
 		IdleTimeout:  15 * time.Second,
 	}
 
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		logger.Fatalf("Error occurred during server start: %v\n", err)
+	if err := server.ListenAndServe(); err != nil {
+		logger.Fatalf("Fatal error occurred: %v\n", err)
 	}
 }
 
@@ -52,72 +52,67 @@ func logging(logger *log.Logger) func(http.Handler) http.Handler {
 	}
 }
 
-func rotHandler() http.Handler {
-	return http.HandlerFunc(rot)
+func apiHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		action, ok := r.URL.Query()["action"]
+		if !ok {
+			http.Error(w, "Action should be specified", http.StatusBadRequest)
+			return
+		}
+		text, ok := r.URL.Query()["text"]
+		if !ok {
+			http.Error(w, "Text should be specified", http.StatusBadRequest)
+			return
+		}
+		key, ok := r.URL.Query()["key"]
+		if !ok {
+			http.Error(w, "Key should be specified", http.StatusBadRequest)
+			return
+		}
+
+
+		switch r.URL.Path {
+		case "/api/v1/rot":
+			parsedKey, _ := strconv.ParseInt(key[0], 10, 0)
+			rotText, err := rot(strings.ToLower(action[0]), text[0], int(parsedKey))
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			fmt.Fprint(w, rotText)
+		case "/api/v1/gronsfeld":
+			gronsfeldText, err := gronsfeld(strings.ToLower(action[0]), text[0], strings.ToLower(key[0]))
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			fmt.Fprint(w, gronsfeldText)
+		default:
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		}
+	})
 }
 
-func gronsfeldHandler() http.Handler {
-	return http.HandlerFunc(gronsfeld)
-}
-
-func rot(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	action, ok := r.URL.Query()["action"]
-	if !ok {
-		http.Error(w, "Action should be specified", http.StatusBadRequest)
-		return
-	}
-	text, ok := r.URL.Query()["text"]
-	if !ok {
-		http.Error(w, "Text should be specified", http.StatusBadRequest)
-		return
-	}
-	key, ok := r.URL.Query()["key"]
-	if !ok {
-		http.Error(w, "Key should be specified", http.StatusBadRequest)
-		return
-	}
-
-	parsedKey, _ := strconv.ParseInt(key[0], 10, 0)
-
-	switch strings.ToLower(action[0]) {
+func rot(action, text string, key int) (string, error) {
+	switch action {
 	case "encrypt":
-		fmt.Fprintf(w, "%q\n", ciphers.Rot(text[0], int(parsedKey), ciphers.ENCRYPT))
+		return ciphers.Rot(text, key, ciphers.ENCRYPT), nil
 	case "decrypt":
-		fmt.Fprintf(w, "%q\n", ciphers.Rot(text[0], int(parsedKey), ciphers.DECRYPT))
+		return ciphers.Rot(text, key, ciphers.DECRYPT), nil
 	default:
-		http.Error(w, "Allowed actions: encrypt, decrypt", http.StatusBadRequest)
+		return "", errors.New("Allowed actions: encrypt, decrypt")
 	}
 }
 
-func gronsfeld(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	action, ok := r.URL.Query()["action"]
-	if !ok {
-		http.Error(w, "Action should be specified", http.StatusBadRequest)
-		return
-	}
-	text, ok := r.URL.Query()["text"]
-	if !ok {
-		http.Error(w, "Text should be specified", http.StatusBadRequest)
-		return
-	}
-	key, ok := r.URL.Query()["key"]
-	if !ok {
-		http.Error(w, "Key should be specified", http.StatusBadRequest)
-		return
-	}
-
-	switch strings.ToLower(action[0]) {
+func gronsfeld(action, text string, key string) (string, error) {
+	switch action {
 	case "encrypt":
-		fmt.Fprintf(w, "%q\n", ciphers.Gronsfeld(text[0], key[0], ciphers.ENCRYPT))
+		return ciphers.Gronsfeld(text, key, ciphers.ENCRYPT), nil
 	case "decrypt":
-		fmt.Fprintf(w, "%q\n", ciphers.Gronsfeld(text[0], key[0], ciphers.DECRYPT))
+		return ciphers.Gronsfeld(text, key, ciphers.DECRYPT), nil
 	default:
-		http.Error(w, "Allowed actions: encrypt, decrypt", http.StatusBadRequest)
+		return "", errors.New("Allowed actions: encrypt, decrypt")
 	}
 }
