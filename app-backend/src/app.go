1c348173c47c40124dee67afabc0330b7818c62b
Nikita Kunets <nik.kunets@gmail.com>
2020-05-17 15:48:39 +0300

    Add simple logging for backend app

--- app-backend/src/app.go
+++ app-backend/src/app.go
@@ -2,20 +2,59 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"./ciphers"
 )
 
 func main() {
-	http.HandleFunc("/v1/rot", rotHandler)
-	http.HandleFunc("/v1/gronsfeld", gronsfeldHandler)
-	http.ListenAndServe(":8080", nil)
+
+	logger := log.New(os.Stdout, "crpt: ", log.LstdFlags)
+	logger.Println("Server starting...")
+
+	router := http.NewServeMux()
+	router.Handle("/v1/rot", rotHandler())
+	router.Handle("/v1/gronsfeld", gronsfeldHandler())
+
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      logging(logger)(router),
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatalf("Error occurred during server start: %v\n", err)
+	}
 }
 
-func rotHandler(w http.ResponseWriter, r *http.Request) {
+func logging(logger *log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				logger.Println(r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
+			}()
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func rotHandler() http.Handler {
+	return http.HandlerFunc(rot)
+}
+
+func gronsfeldHandler() http.Handler {
+	return http.HandlerFunc(gronsfeld)
+}
+
+func rot(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
@@ -47,7 +86,7 @@ func rotHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func gronsfeldHandler(w http.ResponseWriter, r *http.Request) {
+func gronsfeld(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
